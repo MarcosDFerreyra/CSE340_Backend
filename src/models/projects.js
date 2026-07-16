f40e4e38c9743db2add46add25e0be56c981c8bc
MarcosDFerreyra <marcosferrey30>
2026-07-16 00:50:19 -0300

    activity week 03 route and query parameters

--- src/models/projects.js
+++ src/models/projects.js
@@ -2,7 +2,7 @@ import db from './db.js'
 
 const getAllProjects = async () => {
     const query = `
-        SELECT p.project_id, p.organization_id, o.name AS organization_name, p.title, p.description, p.location, p.project_date
+        SELECT p.project_id, p.organization_id, o.name AS organization_name, p.title, p.description, p.location, p.starting_date
       FROM public.projects p
       join organizations o
       on p.organization_id = o.organization_id;
@@ -13,4 +13,24 @@ const getAllProjects = async () => {
     return result.rows;
 }
 
-export { getAllProjects }
+const getProjectsByOrganizationId = async (organizationId) => {
+  const query = `
+        SELECT
+          project_id,
+          organization_id,
+          title,
+          description,
+          location,
+          starting_date
+        FROM projects
+        WHERE organization_id = $1
+        ORDER BY starting_date;
+      `;
+
+  const queryParams = [organizationId];
+  const result = await db.query(query, queryParams);
+
+  return result.rows;
+};
+
+export { getAllProjects, getProjectsByOrganizationId }
