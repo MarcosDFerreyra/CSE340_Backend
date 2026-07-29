f93b6446319200298a8283f55c8999f28d6c4cbd
MarcosDFerreyra <marcosferrey30>
2026-07-29 02:22:01 -0300

    final activity semana 4

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,7 @@ import {
     processNewOrganizationForm,
     organizationValidation} from './controllers/organizations.js';
 import { showProjectsPage, showProjectsDetailPage, showNewProjectForm, processNewProjectForm, projectValidation } from './controllers/projects.js';
-import { showCategoriesPage, showCategoryDetails } from './controllers/categories.js';
+import { showCategoriesPage, showCategoryDetails, showAssignCategoriesForm, processAssignCategoriesForm } from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
 
 const router = express.Router();
@@ -22,15 +22,18 @@ router.get('/categories', showCategoriesPage);
 router.get('/organization/:id', showOrganizationDetailsPage);
 router.get('/project/:id', showProjectsDetailPage);
 router.get('/category/:id', showCategoryDetails);
-router.get('/new-organization', showNewOrganizationForm);
 // Route to handle new organization form submission
+router.get('/new-organization', showNewOrganizationForm);
 router.post('/new-organization', organizationValidation, processNewOrganizationForm);
-router.get('/edit-organization/:id', showEditOrganizationForm);
 // Route to handle the edit organization form submission
+router.get('/edit-organization/:id', showEditOrganizationForm);
 router.post('/edit-organization/:id', organizationValidation, processEditOrganizationForm);
-router.get('/new-project', showNewProjectForm);
 // Route to handle new project form submission
+router.get('/new-project', showNewProjectForm);
 router.post('/new-project', projectValidation, processNewProjectForm);
+// Routes to handle the assign categories to project form
+router.get('/assign-categories/:projectId', showAssignCategoriesForm);
+router.post('/assign-categories/:projectId', processAssignCategoriesForm);
 
 // error-handling routes
 router.get('/test-error', testErrorPage);
